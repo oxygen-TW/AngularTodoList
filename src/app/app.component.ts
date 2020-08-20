25b2975d940f1fc51a03caf26b1b29e397614e46
oxygen-TW <howard2805@gmail.com>
2020-08-20 21:13:33 +0800

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  title = 'myToDo';
+  inputPlaceHolder = "What needs to be done??";
+  
+  showInput(event){
+    console.log(event);
+    event.target.value = "";
+  }
 }
