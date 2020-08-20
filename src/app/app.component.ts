b406f13ccf36367dbc098016417f49d791e48ac6
oxygen-TW <howard2805@gmail.com>
2020-08-20 22:48:58 +0800

    Add some new feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+//純開發使用
+interface ToDo{
+  item: string,
+  isCompleted: boolean
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,7 +17,7 @@ export class AppComponent {
   title = 'myToDo';
   inputPlaceHolder = "What needs to be done??";
   newTodo = "default Value";
-  todoList = [
+  todoList :ToDo[] = [
     {
       item: "Buy Milk",
       isCompleted: false
@@ -21,18 +27,35 @@ export class AppComponent {
       isCompleted: false
     }
   ];
-
+  todoCount = 0;
+  
   AddItem(event){
-    console.log(event);
-    this.todoList.push({item: event.target.value, isCompleted: false})
+    //console.log(event);
+    this.todoList.push({
+      item: event.target.value, 
+      isCompleted: false})
     event.target.value = "";
   }
 
-  RemoveItem(index){
-    this.todoList.splice(index, 1);
+  RemoveItem(idx){
+    this.todoList.splice(idx, 1);
+  }
+
+  AllComplete(){
+    this.todoList.forEach(todo =>  {
+      todo.isCompleted = true;
+   });
   }
 
   RemoveCompleted(){
 
+  }
+
+  ChangeStatus(todo: ToDo){
+    todo.isCompleted = !todo.isCompleted;
+  }
+
+  changeCount(){
+    
   }
 }
