c3908536cb0dc66ca4e80b019f04df978946cfe8
oxygen-TW <howard2805@gmail.com>
2020-08-20 21:52:05 +0800

    Add "add item" and "remove item" and "new item"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'myToDo';
   inputPlaceHolder = "What needs to be done??";
-  
-  showInput(event){
+  newTodo = "default Value";
+  todoList = [
+    {
+      item: "Buy Milk",
+      isCompleted: false
+    },
+    {
+      item: "Work",
+      isCompleted: false
+    }
+  ];
+
+  AddItem(event){
     console.log(event);
+    this.todoList.push({item: event.target.value, isCompleted: false})
     event.target.value = "";
+  }
+
+  RemoveItem(index){
+    this.todoList.splice(index, 1);
+  }
+
+  RemoveCompleted(){
+
   }
 }
