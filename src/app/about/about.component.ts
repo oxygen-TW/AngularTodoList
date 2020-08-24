c653d95114b309784cc241a871d16cc60e35db49
oxygen-TW <howard2805@gmail.com>
2020-08-24 22:00:38 +0800

    show API data to HTML(practice)

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from "@angular/router";
 import { HttpClient } from '@angular/common/http';
+import { User } from "../models/user.model";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: 'app-about',
@@ -8,16 +10,20 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  users = [];
 
   constructor(private router: Router, private route: ActivatedRoute, private HttpClient: HttpClient) { }
 
   gotoTodo(){
     this.router.navigate(['/'], { relativeTo: this.route});
   }
+  
   ngOnInit(): void {
     //Json會自動轉成物件
-    this.HttpClient.get("https://jsonplaceholder.typicode.com/users").subscribe({
-      next: data => console.log(data)
+    this.HttpClient.get<User[]>("https://jsonplaceholder.typicode.com/users").pipe(
+      map(users => users.map(x => ({id: x.id, name: x.name})))
+    ) .subscribe({
+      next: data => this.users = data
     });
   }
 
