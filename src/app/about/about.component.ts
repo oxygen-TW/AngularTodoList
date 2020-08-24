37a46c73ff9b40ecac854249112c887bf22354fb
oxygen-TW <howard2805@gmail.com>
2020-08-24 22:58:22 +0800

    Add template form part. I TOTALLY can't understand....

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from "@angular/router";
 import { HttpClient } from '@angular/common/http';
 import { User } from "../models/user.model";
-import { map,exhaustMap, switchMap, concatMap, mergeMap } from "rxjs/operators";
+import { map, exhaustMap, switchMap, concatMap, mergeMap, filter } from "rxjs/operators";
 // - , 保留第一次
 
 @Component({
@@ -11,29 +11,37 @@ import { map,exhaustMap, switchMap, concatMap, mergeMap } from "rxjs/operators";
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-  users = [];
-  user;
+  users = this.HttpClient.get<User[]>("https://jsonplaceholder.typicode.com/users").pipe(
+    map(users => users.map(x => ({ id: x.id, name: x.name })))
+  );
+  user = this.route.queryParamMap.pipe(
+    map(params => params.get('userId')),
+    filter(userId => userId != null),
+    switchMap(userId => this.HttpClient.get<User>(`https://jsonplaceholder.typicode.com/users/${userId}`))
+  );
 
   constructor(private router: Router, private route: ActivatedRoute, private HttpClient: HttpClient) {
-    this.route.queryParamMap.pipe(
-      map(params => params.get('userId')),
-      switchMap(userId => this.HttpClient.get<User>(`https://jsonplaceholder.typicode.com/users/${userId}`))
-      ).subscribe({
-        next: user => this.user = user //HTTP GET 回來的結果
-      })
-   }
+    //   this.route.queryParamMap.pipe(
+    //     map(params => params.get('userId')),
+    //     switchMap(userId => this.HttpClient.get<User>(`https://jsonplaceholder.typicode.com/users/${userId}`))
+    //     ).subscribe({
+    //       next: user => this.user = user //HTTP GET 回來的結果
+    //     })
+  }
 
-  gotoTodo(){
-    this.router.navigate(['/'], { relativeTo: this.route});
+  save(formGroup){
+    console.log(formGroup);
+  }
+  gotoTodo() {
+    this.router.navigate(['/'], { relativeTo: this.route });
   }
 
   ngOnInit(): void {
     //Json會自動轉成物件
-    this.HttpClient.get<User[]>("https://jsonplaceholder.typicode.com/users").pipe(
-      map(users => users.map(x => ({id: x.id, name: x.name})))
-    ) .subscribe({
-      next: data => this.users = data
-    });
+    //   this.HttpClient.get<User[]>("https://jsonplaceholder.typicode.com/users").pipe(
+    //     map(users => users.map(x => ({id: x.id, name: x.name})))
+    //   ) .subscribe({
+    //     next: data => this.users = data
+    //   });
   }
-
 }
